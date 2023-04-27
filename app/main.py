b106618d0e83206ee29c4757f89b5984b9d48508
Phil Hunt <calihunlax@gmail.com>
2023-04-27 01:04:32 +0100

    can now run on different port from the command line

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 # main.py = main module for CatWiki
 
 import datetime
+import argparse
 
 from flask import request, redirect
+
+from ulib.butil import prn
 
 import allpages
 from allpages import *
@@ -17,8 +20,30 @@ import history
 #---------------------------------------------------------------------
 
 if __name__ == '__main__':
-    print("Starting CatWiki (p3)...")
-    app.run(port=config.PORT, debug=True, threaded=True)
+
+    parser = argparse.ArgumentParser(description="""\
+Start catwiki_p3
+""")
+    parser.add_argument("-d", "--debug",
+        help="Run in debugging mode",
+        action="store_true")
+    parser.add_argument("-p", "--port",
+        help="Port number to use",
+        type=int,
+        default=config.PORT)
+    parser.add_argument("-v", "--verbose",
+        help="Make output more verbose",
+        action="store_true")
+    commandLineArgs = parser.parse_args()
+    prn("commandLineArgs=%r" % (commandLineArgs,))
+    host = "127.0.0.1" if commandLineArgs.debug else "0.0.0.0"
+
+    app.run(host=host,
+        port=commandLineArgs.port,
+        debug=commandLineArgs.debug)
+    #print("Starting CatWiki (p3)...")
+
+    #app.run(port=config.PORT, debug=True, threaded=True)
 
 
 #end
